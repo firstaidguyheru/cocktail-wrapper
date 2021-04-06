40ec2faee477f8bcf3ffe5fac70c2d29df2b0acd
Heru <jheru711@Gmail.com>
2021-04-06 13:33:09 -0400

    Took off session function

--- cocktail/requests.py
+++ cocktail/requests.py
@@ -2,12 +2,11 @@ import aiohttp
 from aiohttp import client_exceptions as errs
 
 class Reqs:
+    ses = aiohttp.ClientSession()
     """Methods to handle requests"""
 
-    @classmethod ## creating one instance for all requests
-    def ses(self):
+    def __init__(self):
         self.ses = aiohttp.ClientSession()
-        return self.ses
 
     @staticmethod ## check for headers
     def header_check(headers: any = None):
@@ -18,7 +17,7 @@ class Reqs:
 
         return headers
 
-    @staticmethod ## check for data {PUT & POST} 
+    @staticmethod ## check for data {POST} 
     def data_check(data: any = None):
         if not data:
             data = {}
@@ -30,7 +29,7 @@ class Reqs:
     @classmethod ## GET request
     async def get(self, url: str, *, headers: any = None):
         headers = Reqs.header_check(headers)
-        ses = Reqs.ses()
+        ses = self.ses
         async with ses.get(url, headers=headers) as r:
             try:
                 if r.status in range(200, 299):
@@ -44,24 +43,22 @@ class Reqs:
                     return data
                 else:
                     print('Endpoint Response: '+r.status)
-        
-        @classmethod ## POST request
-        async def post(self, url: str, *, headers: any = None, data: any = None):
-            headers = Reqs.header_check(headers)
-            data = Reqs.data_check(data)
-            ses = Reqs.ses()
-            async with ses.post(url, headers=headers, data=data) as r:
-                try:
-                    if r.status in range(200, 299):
-                        data = await r.json()
-                        return data
-                    else:
-                        print('Endpoint Response: '+r.status)
-                except errs.ContentTypeError:
-                    if r.status in range(200, 299):
-                        data = await r.json(content_type="text/html")
-                        return data
-                    else:
-                        print('Endpoint Response: '+r.status)
-
-
+    
+    @classmethod ## POST request
+    async def post(self, url: str, *, headers: any = None, data: any = None):
+        headers = Reqs.header_check(headers)
+        data = Reqs.data_check(data)
+        ses = self.ses
+        async with ses.post(url, headers=headers, data=data) as r:
+            try:
+                if r.status in range(200, 299):
+                    data = await r.json()
+                    return data
+                else:
+                    print('Endpoint Response: '+r.status)
+            except errs.ContentTypeError:
+                if r.status in range(200, 299):
+                    data = await r.json(content_type="text/html")
+                    return data
+                else:
+                    print('Endpoint Response: '+r.status)
