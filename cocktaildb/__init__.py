8fd16e67506d81af6fae882f39c02b0a134b9833
Heru J <jheru711@gmail.com>
2021-04-11 22:55:14 -0400

    Rename requests file'

--- cocktaildb/__init__.py
+++ cocktaildb/__init__.py
@@ -1,9 +1,7 @@
-import aiohttp
-from requests import Reqs
-from cocktaildb import search, categories, ingredients, glasses
+import cocktaildb
 
 async def tequila_info():
-  return await search(query='tequila')
+  return await cocktaildb.search(query='tequila')
 
 async def list_of_categories():
   return await cocktaildb.categories()
